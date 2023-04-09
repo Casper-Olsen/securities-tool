3fe7bb3d5517c069f0e1d317d19c0f9e8427ae3a
Casper Olsen <gemic@live.dk>
2023-04-09 21:05:42 +0200

    Added colored output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub mod securities;
 
+use colored::Colorize;
 use std::env;
 
 use crate::securities::commandline::commandline::get_security;
@@ -9,11 +10,11 @@ async fn main() -> Result<(), reqwest::Error> {
     let args = env::args().collect::<Vec<String>>();
     let security = get_security(&args);
 
-    println!("Finding price for security: {}", security);
+    println!("Finding price for security: {}", security.bold());
 
     let price = securities::alphavantage::get_security_price(&security).await?;
 
-    println!("Price for the security {} is: {}", security, price);
+    println!("Price for {} is: {}", security.bold(), price.green());
 
     Ok(())
 }
