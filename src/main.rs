9d6b2a0e4297c64db59e2133a3f57f86cb5ab99d
Casper Olsen <gemic@live.dk>
2023-04-10 10:20:25 +0200

    Args error handling

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,28 @@ use std::env;
 use crate::securities::commandline::get_security;
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
+async fn main() {
     let args = env::args().collect::<Vec<String>>();
-    let security = get_security(&args);
+
+    let security_result = get_security(&args);
+    let security = match security_result {
+        Ok(s) => s,
+        Err(error) => {
+            println!("Args error: {}", error);
+            return;
+        }
+    };
 
     println!("Finding price for security: {}", security.bold());
 
-    let price = securities::alphavantage::get_security_price(&security).await?;
+    let price_result = securities::alphavantage::get_security_price(&security).await;
+    let price = match price_result {
+        Ok(p) => p,
+        Err(error) => {
+            println!("Security not found: {}", error);
+            return;
+        }
+    };
 
     println!("Price for {} is: {}", security.bold(), price.green());
-
-    Ok(())
 }
--- src/securities/commandline.rs
+++ src/securities/commandline.rs
@@ -1,8 +1,21 @@
-pub fn get_security(args: &Vec<String>) -> String {
+use std::{error::Error, fmt};
+
+#[derive(Debug)]
+pub struct ArgsError;
+
+impl Error for ArgsError {}
+
+impl fmt::Display for ArgsError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "No security specified")
+    }
+}
+
+pub fn get_security(args: &Vec<String>) -> Result<String, ArgsError> {
     if args.len() < 2 {
-        panic!("No security specified - Stopping...") // TODO: Add error handling
+        return Err(ArgsError {});
     }
 
     let security = &args[1];
-    String::from(security)
+    Ok(String::from(security))
 }
